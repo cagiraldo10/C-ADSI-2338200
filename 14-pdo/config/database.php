8e2c2e68c7de078182269738fecb5346ac8a739f
cagiraldo10 <carlosgiraldo102000@gmail.com>
2022-09-19 11:52:28 -0500

    new files

--- 14-pdo/config/database.php
+++ 14-pdo/config/database.php
@@ -1,11 +1,136 @@
 <?php
-
     try {
-        $conx = new PDO("mysql:host=$host;dbname=$name_db",$user,$password);
-        $conx ->exec("set names utf8");
-
-        echo "Connection Successfully!";
+        $conx = new PDO("mysql:host=$host;dbname=$name_db",$user,$passwd);
+        $conx->exec("set names utf8");
+        //echo "Connection Successfully!";
     }
     catch(PDOException $e) {
-        echo "Connection Error: " .$e->getMessage();
+        echo "Connection Error: " . $e->getMessage();
+    }
+
+    //-- CRUD => Pokemons
+
+    // Insert Pokemon
+    function addPokemon($conx, $name, $type, $strength, $stamina, $speed, $accuracy, $image, $trainer_id) {
+        try {
+            $sql = "INSERT INTO pokemons (name, type, strength, stamina, speed, accuracy,
+                    image, trainer_id) VALUES (:name, :type, :strength, :stamina, :speed, 
+                    :accuracy, :image, :trainer_id)";
+            $stm = $conx->prepare($sql);
+            $stm->bindparam(":name", $name);
+            $stm->bindparam(":type", $type);
+            $stm->bindparam(":strength", $strength);
+            $stm->bindparam(":stamina", $stamina);
+            $stm->bindparam(":speed", $speed);
+            $stm->bindparam(":accuracy", $accuracy);
+            $stm->bindparam(":image", $image);
+            $stm->bindparam(":trainer_id", $trainer_id);
+            if($stm->execute()) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+
+    }
+
+    // Update Pokemon
+    function updatePokemon($conx, $id, $name, $type, $strength, $stamina, $speed, $accuracy, $image, $trainer_id) {
+        try {
+            if($image != null) {
+                $sql = "UPDATE pokemons SET name = :name, type = :type, strength = :strength, 
+                        stamina = :stamina, speed = :speed, accuracy = :accuracy, image = :image, 
+                        trainer_id = :trainer_id WHERE id = :id ";
+            } else {
+                $sql = "UPDATE pokemons SET name = :name, type = :type, strength = :strength, 
+                        stamina = :stamina, speed = :speed, accuracy = :accuracy, 
+                        trainer_id = :trainer_id WHERE id = :id ";
+            }
+                
+            $stm = $conx->prepare($sql);
+            $stm->bindparam(":id", $id);
+            $stm->bindparam(":name", $name);
+            $stm->bindparam(":type", $type);
+            $stm->bindparam(":strength", $strength);
+            $stm->bindparam(":stamina", $stamina);
+            $stm->bindparam(":speed", $speed);
+            $stm->bindparam(":accuracy", $accuracy);
+            if($image != null) {
+                $stm->bindparam(":image", $image);
+            }
+            $stm->bindparam(":trainer_id", $trainer_id);
+            if($stm->execute()) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+
+    }
+
+
+    // List All Pokemons
+    function listAllPokemons($conx) {
+        try {
+            $sql = "SELECT p.*, t.name AS nametrainer 
+                    FROM pokemons AS p, trainers AS t
+                    WHERE p.trainer_id = t.id
+                    ORDER BY p.id ASC";
+            $stm = $conx->prepare($sql);
+            $stm->execute();
+            return $stm->fetchAll();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    // Show Pokemon
+    function showPokemon($conx, $id) {
+        try {
+            $sql = "SELECT p.*, t.name AS nametrainer 
+                    FROM pokemons AS p, trainers AS t
+                    WHERE p.id = :id AND p.trainer_id = t.id";
+            $stm = $conx->prepare($sql);
+            $stm->bindparam(":id", $id);
+            $stm->execute();
+            return $stm->fetchAll();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+
+    // Delete Pokemon
+    function deletePokemon($conx, $id) {
+        try {
+            $sql = "DELETE FROM pokemons WHERE id = :id";
+            $stm = $conx->prepare($sql);
+            $stm->bindparam(":id", $id);
+            if($stm->execute()) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+
+    }
+
+    // Trainers
+
+    // List All Trainers
+    function listAllTrainers($conx) {
+        try {
+            $sql = "SELECT * FROM trainers";
+            $stm = $conx->prepare($sql);
+            $stm->execute();
+            return $stm->fetchAll();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
     }
--- 14-pdo/index.php
+++ 14-pdo/index.php
@@ -1,4 +1,5 @@
 <?php require 'config/app.php' ?>
+<?php include('config/database.php') ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,32 +10,102 @@
     <link rel="stylesheet" href="public/css/bootstrap-reboot.min.css">
     <link rel="stylesheet" href="public/css/bootstrap.min.css">
     <link rel="stylesheet" href="public/css/fontawesome.min.css">
+    <link rel="stylesheet" href="public/css/fonts.css">
 </head>
 <body>
     <main class="container">
         <section class="row">
-            <div class="col-8 offset-2">
+            <div class="col-md-8 offset-md-2 my-5">
                 <h1 class="text-center">
-                    <i class="fa-solid fa-person-swimming"></i>
+                    <i class="fa fa-dragon"></i>
                     Web App Pokemons
                 </h1>
                 <hr>
-                <?php include 'config/database.php' ?>
+                <a href="add.php" class="btn btn-lg btn-outline-success">
+                    <i class="fa fa-plus"></i>
+                    Add Pokemon
+                </a>
+                <div class="table-responsive">
+                    <table class="table table-striped table-hover mt-5">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>NAME</th>
+                                <th class="d-none d-sm-table-cell">TYPE</th>
+                                <th>IMAGE</th>
+                                <th class="d-none d-sm-table-cell">TRAINER</th>
+                                <th>ACTIONS</th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-group-divider">
+                            <?php $pokemons = listAllPokemons($conx) ?>
+                            <?php foreach($pokemons as $pokemon): ?>
+                            <tr>
+                                <td><?php echo $pokemon['id'] ?></td>
+                                <td><?php echo $pokemon['name'] ?></td>
+                                <td class="d-none d-sm-table-cell"><span class="badge bg-dark"><?php echo $pokemon['type'] ?></span></td>
+                                <td>
+                                    <img src="<?php echo $pokemon['image'] ?>" width="40px">
+                                </td>
+                                <td class="d-none d-sm-table-cell"><?php echo $pokemon['nametrainer'] ?></td>
+                                <td>
+                                    <div class="dropdown">
+                                        <button class="btn btn-dark dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                                            <i class="fa fa-cog"></i>
+                                        </button>
+                                        <ul class="dropdown-menu dropdown-menu-dark">
+                                            <li><a class="dropdown-item" href="show.php?id=<?php echo $pokemon['id'] ?>"><i class="fa fa-search"></i> Show</a></li>
+                                            <li><a class="dropdown-item" href="edit.php?id=<?php echo $pokemon['id'] ?>"><i class="fa fa-pen"></i> Edit</a></li>
+                                            <li><a class="dropdown-item bg-danger btn-delete" data-id="<?php echo $pokemon['id'] ?>" href="javascript:;"><i class="fa fa-trash"></i> Delete</a></li>
+                                        </ul>
+                                    </div>
+                                </td>
+                            </tr>
+                            <?php endforeach ?>
+                        </tbody>
+                    </table>
+                </div>
+                
             </div>
         </section>
+        <?php $conx = null; ?>
     </main>
     <script src="public/js/jquery-3.6.0.min.js"></script>
     <script src="public/js/bootstrap.min.js"></script>
+    <script src="public/js/bootstrap.bundle.min.js"></script>
     <script src="public/js/sweetalert2.js"></script>
     <script>
         $(document).ready(function () {
-            Swal.fire(
-                'Good job!',
-                'Everything is OK!',
-                'success'
-
-            )
-        });
+            <?php if(isset($_SESSION['message'])): ?>
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: '<?php echo $_SESSION['message'] ?>',
+                showConfirmButton: false,
+                timer: 2500
+            })
+            <?php endif ?>
+            <?php unset($_SESSION['message']) ?>
+            // - - - - - - - - - - -
+            $('.btn-delete').click(function (e) {
+                e.preventDefault()
+                $id = $(this).attr('data-id')
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#212529',
+                    cancelButtonColor: '#dc3545',
+                    confirmButtonText: 'Yes, delete it!'
+                    }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.replace('delete.php?id=' + $id)
+                    }
+                })
+            })
+            // - - - - - - - - - -
+        })
     </script>
 </body>
 </html>
